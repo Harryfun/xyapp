7291a950cbc4573583eeda9e27e21c3ae8c4d111
you@example.com <you@example.com>
2019-07-29 15:54:46 +0800

    配置rem以及viewport 导入静态资源

--- src/main.js
+++ src/main.js
@@ -2,7 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入flexible适配 会自动加上viewport
+import 'amfe-flexible/index.js'
+// 按需引入vant组件
+import {
+  DatetimePicker,
+  Button,
+  List
+} from 'vant'
 
+Vue.use(DatetimePicker)
+  .use(Button)
+  .use(List)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <a href="#">
+      我是谁？
+      <span>我是皮卡丘Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores cupiditate dolorem adipisci in quae magnam mollitia culpa dolorum necessitatibus eligendi tenetur voluptate, dicta error quia amet, laborum omnis hic aliquid!</span>
+    </a>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      currentDate: new Date()
+    }
+  },
+  mounted () {
+    window.console.log(1)
   }
 }
 </script>
+
+<style lang="less" scoped>
+a {
+  display: block;
+  width: 375px/2;
+  height: 375px;
+  background-color: #0a0;
+  span {
+    font-size: 20px;
+  }
+}
+</style>
